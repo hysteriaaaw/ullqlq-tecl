12ad7c177e88529486f8969353e150342f8c42c6
hysteriaaaw <davi.moyses@escola.pr.gov.br>
2026-04-15 07:42:26 -0300

    Add files via upload

---
+++ me  wow wooowo meow/blllub.js
@@ -0,0 +1,49 @@
+// 1: exiba os numeros de 1 a 10 usando for
+for (let i = 1; i<=10; i++){
+    console.log(i);
+}
+
+// 2: exiba os numeros de 10 até 1 em ordem decrescente 
+for (let y = 10; y>=1; y--){
+    console.log(y);
+}
+
+// 3: exiba todos os numeros pares de 2 a 20 
+for(let f = 2; f<=20; f= f + 2){
+    console.log(f);
+}
+// 4; exiba a tabuada do numero 5 (5x1 até 5x10);
+for(let f = 1; f<=10; f++){
+    console.log(5 + "x" + f + "=" + 5*f);
+}
+// 5: calcule e exiba a soma dos numeros de 1 até 50;
+let soma = 0
+for(let f = 1; f<=50; f++){
+    soma = soma + f;
+    console.log(`A soma é: ${soma}`);
+}
+
+// 6: conte de 1 até o numero 8 (use for)
+for (let i = 1; i<=8; i++){
+    console.log(i);
+}
+
+// 7: exiba todos os numeros ímpares de 1 a 15;
+for(let f = 1; f<=15; f= f + 2){
+    console.log(f);
+}
+
+// 8: multiple os numeros de 1 a 10 por 2;
+for(let f = 1; f<=10; f++){
+    console.log(f + "x" + 2 + "=" + 2*f);
+}
+
+// 9: some os numeros de 5 em 5 até 50;
+for (let i = 5; i<=50; i=i+5)
+    console.log(i);
+
+// 10: calcule e exiba o quadrado dos numeros de 1 a 10
+for(let f = 1; f<=10; f++){
+    console.log(f + "x" + f + "=" + f*f);
+}
+
